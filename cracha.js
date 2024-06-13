c0d3215f4c4c98283f5265791955e490bf716ca9
EDMUNDO JOSE BATISTA <eddmundosjb@gmail.com>
2024-06-13 20:11:13 -0300

    feat: varias melhorias implementadas com e sem css has

--- cracha.js
+++ cracha.js
@@ -1,47 +1,51 @@
-function printCard(){
-    (function() {
-      var element = document.getElementById("principal");
-      var btnPrint = document.getElementById("print");
-      var container = document.getElementById("card");
-      
-      var beforePrint = function() {
-          console.log('Antes de imprimir...');
-          btnPrint.classList.add("hide");
-          element.classList.remove("center");
-          element.classList.add("margin");
-          document.body.classList.remove("beauty");
-          container.classList.remove("box-shadow");
-      };
-  
-      var afterPrint = function() {
-          console.log('Depois de imprimir...');
-          btnPrint.classList.remove("hide");
-          element.classList.add("center");
-          element.classList.remove("margin");
-          document.body.classList.add("beauty");
-          container.classList.add("box-shadow");
-      };
-  
-      if (window.matchMedia) {
-          var mediaQueryList = window.matchMedia('print');
-          mediaQueryList.addListener(function(mql) {
-              if (mql.matches) {
-                  beforePrint();
-              } else {
-                  afterPrint();
-              }
-          });
-      }
-  
-      window.onbeforeprint = beforePrint;
-      window.onafterprint = afterPrint;  
-  }());
-    print();
-}
+function printCard() {
+  (function () {
+    var element = document.getElementById("principal");
+    var btnPrint = document.getElementById("print");
+    const container = document.querySelector("#card");
+    var boxes = document.querySelectorAll(".box");
 
-function removeDashed(){
-  var label = document.getElementById("label__picture");
-    label.classList.remove("border");
+    var beforePrint = function () {
+      console.log("Antes de imprimir...");
+      btnPrint.classList.add("hide");
+      element.classList.remove("center");
+      element.classList.add("margin");
+      document.body.classList.remove("beauty");
+      container.classList.remove("box-shadow");
+
+      boxes.forEach(box => {
+        box.classList.add("printer");
+      });
+    };
+
+    var afterPrint = function () {
+      console.log("Depois de imprimir...");
+      btnPrint.classList.remove("hide");
+      element.classList.add("center");
+      element.classList.remove("margin");
+      document.body.classList.add("beauty");
+      container.classList.add("box-shadow");
+
+      boxes.forEach(box => {
+        box.classList.remove("printer");
+      });
+    };
+
+    if (window.matchMedia) {
+      var mediaQueryList = window.matchMedia("print");
+      mediaQueryList.addListener(function (mql) {
+        if (mql.matches) {
+          beforePrint();
+        } else {
+          afterPrint();
+        }
+      });
+    }
+
+    window.onbeforeprint = beforePrint;
+    window.onafterprint = afterPrint;
+  })();
+  print();
 }
 
 // picture //
@@ -51,25 +55,27 @@ const pictureImageTxt = "Escolha uma imagem";
 pictureImage.innerHTML = pictureImageTxt;
 
 inputFile.addEventListener("change", function (e) {
-const inputTarget = e.target;
-const file = inputTarget.files[0];
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
 
-if (file) {
-const reader = new FileReader();
+  if (file) {
+    const reader = new FileReader();
 
-reader.addEventListener("load", function (e) {
-const readerTarget = e.target;
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
 
-const img = document.createElement("img");
-img.src = readerTarget.result;
-img.classList.add("picture__img");
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
 
-pictureImage.innerHTML = "";
-pictureImage.appendChild(img);
-});
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
 
-reader.readAsDataURL(file);
-} else {
-pictureImage.innerHTML = pictureImageTxt;
-}
+    reader.readAsDataURL(file);
+    document.getElementById("label__picture").classList.remove("border");
+  } else {
+    reader.readAsDataURL(file);
+    pictureImage.innerHTML = pictureImageTxt;
+  }
 });
